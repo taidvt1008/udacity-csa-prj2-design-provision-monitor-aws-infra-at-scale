f3efe66c6ddb12c879c0366887e6427794610a7f
Tai Dao <dvtientai@gmail.com>
2023-09-03 08:07:53 +0700

    update terraform to provide EC2 instance name

--- Task5-Use Terraform to Provision AWS Infrastructure/Exercise_1/main.tf
+++ Task5-Use Terraform to Provision AWS Infrastructure/Exercise_1/main.tf
@@ -7,10 +7,16 @@ resource "aws_instance" "Udacity_T2" {
   count         = 4
   ami           = "ami-051f7e7f6c2f40dc1" # Amazon Linux 2023 AMI (64-bit (x86))
   instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
 }
 
 resource "aws_instance" "Udacity_M4" {
   count         = 2
   ami           = "ami-051f7e7f6c2f40dc1" # Amazon Linux 2023 AMI (64-bit (x86))
   instance_type = "m4.large"
+  tags = {
+    Name = "Udacity M4"
+  }
 }
